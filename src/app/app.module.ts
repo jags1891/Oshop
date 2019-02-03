adc27f75beefb4150fded0cd76797dab3b669181
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-02-03 03:31:17 -0800

    Deploy to Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AngularFireModule}  from 'angularfire2';
 import {AngularFireDatabaseModule}  from 'angularfire2/database';
 import {AngularFireAuthModule}  from 'angularfire2/auth';
 import {RouterModule} from '@angular/router'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,7 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from  './login/login.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { LoginComponent } from './login/login.component';
     OrderSuccessComponent,
     MyOrdersComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -38,12 +40,14 @@ import { LoginComponent } from './login/login.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       {path:'', component: HomeComponent},
       {path:'products', component: ProductsComponent},
       {path:'shopping-cart', component: ShoppingCartComponent},
       {path:'check-out', component: CheckOutComponent},
       {path:'order-success', component: OrderSuccessComponent},
+      {path:'my/orders', component: MyOrdersComponent},
       {path:'login', component: LoginComponent},
       {path:'admin/products', component: AdminProductsComponent},
       {path:'admin/orders', component: AdminOrdersComponent},
