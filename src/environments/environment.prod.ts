cab71003a48af09a152fbfbea55de9fe961456af
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-02-03 00:14:55 -0800

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGzuzAVAGTIFKc1ljpmhXjhtWPGRRYdOM",
+    authDomain: "oshop-e8670.firebaseapp.com",
+    databaseURL: "https://oshop-e8670.firebaseio.com",
+    projectId: "oshop-e8670",
+    storageBucket: "oshop-e8670.appspot.com",
+    messagingSenderId: "906494491444"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGzuzAVAGTIFKc1ljpmhXjhtWPGRRYdOM",
+    authDomain: "oshop-e8670.firebaseapp.com",
+    databaseURL: "https://oshop-e8670.firebaseio.com",
+    projectId: "oshop-e8670",
+    storageBucket: "oshop-e8670.appspot.com",
+    messagingSenderId: "906494491444"
+  }
 };
 
 /*
